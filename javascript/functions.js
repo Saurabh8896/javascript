7fcf91aeeb2d62c94c34ceeb93ee30aba1182806
Saurabh8896 <107997131+Saurabh8896@users.noreply.github.com>
2024-08-01 15:06:32 +0000

    IIFE

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,2 +1,13 @@
-console.log("HELLO")
-console.log("Check commit function")
+// console.log("HELLO")
+// console.log("Check commit function")
+(function chai(){
+    console.log("db connected")
+})();
+
+(() => {
+    console.log("Hello connected")
+})();
+
+((name)=> {
+    console.log(`hello ${name}`)
+})('my name is saurabh')
